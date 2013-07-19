096f79cd78a03abe6c0993769604802ac9cf6ca5
Steve Ratcliffe <sr@itinken.com>
2013-07-19 11:41:12 +0100

    The extra_checkbox field editor. It is a simple checkbox, unlabeled.

---
+++ js/checkbox.js
@@ -0,0 +1,38 @@
+/**
+ * A plain checkbox implementation.
+ *
+ * @type {{makeField: Function, value: Function, displayValue: Function}}
+ */
+$.fn.jinplace.editors.extra_checkbox = {
+
+    makeField: function(element, data) {
+        this.blurAction = 'submit';
+
+        var choices;
+
+        if (data.charAt(0) == '[')
+            choices = $.parseJSON(data);
+
+        if (!choices || choices.length < 2)
+            choices = ["No", "Yes"];
+
+        this.choices = choices;
+
+        var f = $('<input type=checkbox>');
+        f.on('click', function(ev) {ev.stopPropagation();});
+
+        // Set the checkbox to the checked state if the text matches the 'true' value.
+        var text = $.trim(element.text());
+        f.attr('checked', text == choices[1]);
+
+        return f;
+    },
+
+    value: function() {
+        return this.inputField.prop('checked')? 1: 0;
+    },
+
+    displayValue: function(data) {
+        return this.choices[data ? 1 : 0];
+    }
+};
---
+++ tests/checkbox.js
@@ -0,0 +1,77 @@
+
+( function() {
+    var span;
+    module("extra_checkbox", {
+            setup: function() {
+                span = $('<span data-type="extra_checkbox">Yes</span>');
+            }
+        }
+    );
+
+    var qfix = $("#qunit-fixture");
+//    function cb_span() { return $('<span data-type="extra_checkbox">Yes</span>'); }
+
+    test("Default checked for yes", 2, function() {
+        span.appendTo(qfix).jinplace();
+
+		equal(span.text(), "Yes");
+
+		span.click();
+		var inp = span.find('input');
+		equal(inp.prop('checked'), true);
+	});
+
+    test("Default unchecked for No", 1, function() {
+        span.text('No').appendTo(qfix).jinplace();
+
+        span.click();
+
+        var inp = span.find('input');
+        equal(inp.prop('checked'), false);
+    });
+
+    test("After blur, no checkbox, correct text", 3, function() {
+        span.appendTo(qfix).jinplace();
+
+        span.click();
+        var inp = span.find('input');
+        equal(inp.prop('checked'), true);
+
+        $('#other').focus();
+
+        // The blur event is processed after a timeout, so we have to wait a bit longer
+        // and then test the assertions.
+        stop();
+        setTimeout(function () {
+            equal(span.find('input').length, 0, 'no input after timeout');
+            equal(span.text(), 'Yes', 'text set');
+
+            start();
+        }, 400);
+
+    });
+
+    test("After change state and blur, text is No", 4, function() {
+        span.appendTo(qfix).jinplace();
+        equal(span.text(), "Yes");
+
+        span.click();
+        var inp = span.find('input');
+        inp.click();
+        equal(inp.prop('checked'), false, 'checkbox deselected');
+
+        $('#other').focus();
+
+        // The blur event is processed after a timeout, so we have to wait a bit longer
+        // and then test the assertions.
+        stop();
+        setTimeout(function () {
+            equal(span.find('input').length, 0, 'no input after timeout');
+            equal(span.text(), 'No', 'text set');
+
+            start();
+        }, 400);
+
+    });
+
+} )();
